138283043a218b02732186e2df5f5c43f2561c49
judawu <59474654+judawu@users.noreply.github.com>
2020-02-16 19:52:36 +0800

    Update passwall.sh

--- passwall.sh
+++ passwall.sh
@@ -1,7 +1,6 @@
-
 #!/bin/sh
 
-cat >&1 <<-'EOF'
+: <<-'EOF'
 Copyright 2020 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
