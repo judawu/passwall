47dabae7d24d668e536042de8c43ca14a2d34367
judawu <59474654+judawu@users.noreply.github.com>
2020-03-19 12:26:58 +0800

    Update passwall.sh

--- passwall.sh
+++ passwall.sh
@@ -271,8 +271,7 @@ get_arch
 	             fi
  fi 
 
- if  ![[ -f /etc/v2ray ]]; then
- 
+ if ![[ -f /etc/v2ray ]]; then
   date -R
   cp /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
   bash <(curl -L -s https://install.direct/go.sh)
