531ed8383d4cfc133028e22adb4543aa04e9baca
judawu <59474654+judawu@users.noreply.github.com>
2020-09-05 19:18:44 +0800

    Update passwall.sh

--- passwall.sh
+++ passwall.sh
@@ -812,7 +812,24 @@ fi
 }
 
 iptable_go() {
-# 设置策略路由
+# 设置wireguard策略路由
+# wg set wg0 fwmark 51820
+# ip -4 route add 0.0.0.0/0 dev wg0 table 51820
+# ip -4 rule add not fwmark 51820 table 51820
+#resolvconf -a wg0  < /etc/wireguard/wgdns.conf
+
+
+
+#iptables -A INPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+#iptables -A FORWARD -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+#iptables -A INPUT -p udp -m udp --dport 51820 -m conntrack --ctstate NEW -j ACCEPT
+#iptables -A INPUT -s 10.200.200.0/24 -p tcp -m tcp --dport 53 -m conntrack --ctstate NEW -j ACCEPT
+#iptables -A INPUT -s 10.200.200.0/24 -p udp -m udp --dport 53 -m conntrack --ctstate NEW -j ACCEPT
+#iptables -A FORWARD -i wg0 -o wg0 -m conntrack --ctstate NEW -j ACCEPT
+#iptables -t nat -A POSTROUTING -s 10.200.200.0/24 -o ens3 -j MASQUERADE
+
+
+
 ip rule add fwmark 1 table 100
 ip route add local 0.0.0.0/0 dev lo table 100
 
